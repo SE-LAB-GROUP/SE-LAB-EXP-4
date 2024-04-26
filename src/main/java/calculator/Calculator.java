e674f7a16f0734e8c9b13054b8b4ad45ef58486f
mehrbarzamini <mehrbarzamini@gmail.com>
2024-04-26 20:00:14 +0330

    feat: Calculator.java class was implemented

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Calculator {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public int exponentiate(int base, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+}
